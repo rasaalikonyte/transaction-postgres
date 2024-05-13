040e99f63d90f6c5160e474d64e5a26f7bf93c91
Your Name <rasa.alikonyte@gmail.com>
2024-05-13 15:49:13 +0300

    db.js updated

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "todo-database",
+  database: "transaction-database",
   password: "postgres",
   port: 5432,
 });
