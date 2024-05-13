2516da8279e081b374293e54e87fd06dd9adb0bd
Your Name <rasa.alikonyte@gmail.com>
2024-05-13 17:46:50 +0300

    updated schema.sql

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "transaction-database",
+  database: "transactions-database",
   password: "postgres",
   port: 5432,
 });
--- schema.sql
+++ schema.sql
@@ -9,3 +9,13 @@ CREATE TABLE IF NOT EXISTS transactions (
     user_from INT REFERENCES users(id),
     user_to INT REFERENCES users(id)
 );
+
+SELECT u.name AS user_name, SUM(t.amount) AS total_amount_received
+FROM users u
+JOIN transactions t ON u.id = t.user_to
+GROUP BY u.name
+ORDER BY total_amount_received DESC
+LIMIT 1;
+
+SELECT SUM(amount) AS total_amount_sent
+FROM transactions;
